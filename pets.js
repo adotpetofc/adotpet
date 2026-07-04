95a05770bad63d376ec1bfe7d24fd81e7ca4d263
adotpetofc <adotpet.ofc@gmail.com>
2026-07-04 17:09:17 -0300

    Update pets.js

--- pets.js
+++ pets.js
@@ -1,1 +1,12 @@
+const API_PETS = "COLE_AQUI_SUA_URL_DO_GOOGLE_SCRIPT";
 
+let pets = [];
+
+async function carregarPets() {
+  const res = await fetch(API_PETS);
+  pets = await res.json();
+
+  iniciarMapa(); // função que cria os markers
+}
+
+carregarPets();
