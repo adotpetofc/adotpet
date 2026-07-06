76c03c169efe4c2bae697c037089a6d2d66f51aa
adotpetofc <adotpet.ofc@gmail.com>
2026-07-06 01:55:38 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -307,7 +307,7 @@ const pets = [
   {
     animal: "Briana",
     contato: "7996701204",
-    foto: "https://github.com/LeonardoSaintAna/imagem_adotpet-/blob/main/Diana.jpeg?raw=true",
+    foto: "https://github.com/adotpetofc/adotpet/blob/main/imagens/briana.png?raw=true",
     lat: -10.773726750041178, 
     lng: -37.83775337580061,
     status: "adotar"
@@ -315,7 +315,7 @@ const pets = [
   {
     animal: "Ariana",
     contato: "7996701204",
-    foto: "https://github.com/LeonardoSaintAna/imagem_adotpet-/blob/main/Diana.jpeg?raw=true",
+    foto: "https://github.com/adotpetofc/adotpet/blob/main/imagens/ariana.png?raw=true",
     lat: ,-10.7736657980013, 
     lng: -37.837700966594916,
     status: "adotar"
