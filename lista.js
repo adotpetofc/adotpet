2951958a9a82f95536b9045ea2ec7e0a451df842
adotpetofc <adotpet.ofc@gmail.com>
2026-09-04 19:27:37 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -2,7 +2,7 @@ const pets = [
   {
     animal: "Neve",
     contato: "7996006391",
-    foto: "https://raw.githubusercontent.com/adotpetofc/adotpet/1c6b9365746df533ef5d3a47084a1204c28f99be/neve.jpeg",
+    foto: "https://github.com/adotpetofc/adotpet/blob/main/neve.jpeg?raw=true",
     lat: -10.910031809634463,   
     lng: -37.69379313768288,
     status: "resgatar",
